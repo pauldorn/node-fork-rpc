76b0fba9f6198eb6063eec9fcf1bf89464f68ff0
Paul Dorn <Paul.Dorn@redlion.net>
2014-02-21 09:00:14 -0600

    Fundamental change.  Api acquisition is now async

--- examples/passedApi.js
+++ examples/passedApi.js
@@ -1,12 +1,16 @@
 
 module.exports = {
-	one: function(doOne, callback) {
-		console.log("One Boo", arguments);
-		callback("One boo calling back");
-	},
-	two: function(doTwo, callback) {
-		console.log("Two Boo", arguments);
-		callback("Two boo calling back");
+	getApi: function(register){
+		register({
+			one: function(doOne, callback) {
+				console.log("One Boo", arguments);
+				callback("One boo calling back");
+			},
+			two: function(doTwo, callback) {
+				console.log("Two Boo", arguments);
+				callback("Two boo calling back");
+			}
+		});
 	}
 };
 
--- index.js
+++ index.js
@@ -16,77 +16,81 @@
 		return functionInfo;
 	}
 
-	function run(api, onReady) {
-		var apiInstance = require(api), child_process = require("child_process");
-		var client = {}, fInfoList = getFunctionInfo(apiInstance), path = require("path"),
-			child = child_process.fork(path.resolve(path.join(__dirname, "lib", "childRunner")), [api]), outstandingCalls = {}, ct = 0;
+	function run(module, onReady) {
+		require(module).getApi(function(api){
+			var child_process = require("child_process"), client = {}, fInfoList = getFunctionInfo(api), path = require("path"),
+				child = child_process.fork(path.resolve(path.join(__dirname, "lib", "childRunner")), [module]), outstandingCalls = {}, ct = 0;
 
 
-		fInfoList.forEach(function(fInfo){
-			client[fInfo.fName] = function(){
-				var args = Array.prototype.slice.call(arguments, 0), callback;
-				if(typeof(args[args.length-1]) === 'function'){
-					callback = args[args.length-1];
-					args = args.slice(0, args.length-1);
-				} else {
-					callback = false;
-				}
-				var e = {
-					fName: fInfo.fName,
-					args: args,
-					msgNum: ct++
-				}
-				outstandingCalls[e.msgNum] = callback;
-				child.send(e);
-			}
-		});
-		child.on("message", function __once(e){
-			child.removeListener("message", __once);
-			child.on("message", function(e){
-				var callback;
-				if(e.msgNum === undefined){
-					console.log("Error: No message number in child response");
-					return;
-				}
-				if(outstandingCalls[e.msgNum] === undefined) {
-					console.log("Error: Wrong message number in child response");
-					return;
-				}
-				callback = outstandingCalls[e.msgNum];
-				delete outstandingCalls[e.msgNum];
-				if(callback) {
-					callback.call(callback, e.args);
+			fInfoList.forEach(function(fInfo){
+				client[fInfo.fName] = function(){
+					var args = Array.prototype.slice.call(arguments, 0), callback;
+					if(typeof(args[args.length-1]) === 'function'){
+						callback = args[args.length-1];
+						args = args.slice(0, args.length-1);
+					} else {
+						callback = false;
+					}
+					var e = {
+						fName: fInfo.fName,
+						args: args,
+						msgNum: ct++
+					}
+					outstandingCalls[e.msgNum] = callback;
+					child.send(e);
 				}
 			});
-			onReady(client);
-		});
+			child.on("message", function __once(e){
+				child.removeListener("message", __once);
+				child.on("message", function(e){
+					var callback;
+					if(e.msgNum === undefined){
+						console.log("Error: No message number in child response");
+						return;
+					}
+					if(outstandingCalls[e.msgNum] === undefined) {
+						console.log("Error: Wrong message number in child response");
+						return;
+					}
+					callback = outstandingCalls[e.msgNum];
+					delete outstandingCalls[e.msgNum];
+					if(callback) {
+						callback.call(callback, e.args);
+					}
+				});
+				onReady(client, child);
+			});
+		})
+
 	}
 
 	// Register's partner (client) is implicit, in that it is always this processes parent process.
 
 	function register(api) {
-		var fInfoList = getFunctionInfo(api), fNames = {};
-		fInfoList.forEach(function (fInfo){
-			// wrap each function so that it can get called from an incoming message.
-			fNames[fInfo.fName] = true;
-		});
-		process.on("message", function(e){
-			if(e.fName === undefined) {
-				console.log("Error, no function name in RPC");
-				return;
-			}
-			if(fNames[e.fName] === undefined) {
-				console.log("Error, unknown function name in RPC");
-				return;
-			}
-			e.args.push(function(){
-				var args = Array.prototype.slice.call(arguments, 0);
-				process.send({
-					msgNum: e.msgNum,
-					args: args
+		api.getApi(function(apiInstance){
+			var fInfoList = getFunctionInfo(apiInstance), fNames = {};
+			fInfoList.forEach(function (fInfo){
+				// wrap each function so that it can get called from an incoming message.
+				fNames[fInfo.fName] = true;
+			});
+			process.on("message", function(e){
+				if(e.fName === undefined) {
+					console.log("Error, no function name in RPC");
+					return;
+				}
+				if(fNames[e.fName] === undefined) {
+					console.log("Error, unknown function name in RPC");
+					return;
+				}
+				e.args.push(function(){
+					var args = Array.prototype.slice.call(arguments, 0);
+					process.send({
+						msgNum: e.msgNum,
+						args: args
+					})
 				})
+				apiInstance[e.fName].apply(null, e.args);
 			})
-			api[e.fName].apply(null, e.args);
 		})
 	}
 
